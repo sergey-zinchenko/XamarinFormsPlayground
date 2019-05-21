3f6f4674a0971db967df53539a31e155b6aa6479
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2019-05-21 14:59:22 +0300

    initial comit

---
+++ CrossLibrary/Application.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrossLibrary
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new MainPage(); // your page here
+        }
+
+        protected override void OnStart()
+        {
+            Console.WriteLine("OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            Console.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Console.WriteLine("OnResume");
+        }
+    }
+}
